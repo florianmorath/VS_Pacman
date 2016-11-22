09d053d3437472ae8ef9cc0cd6245ce808e11be0
Linus <linusfessler@gmail.com>
2016-11-22 16:58:14 +0100

    Renamed things, added activities

---
+++ Pac-Man/android/src/ch/ethz/inf/vs/a4/fmorath/pac_man/activities/GameActivity.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.a4.fmorath.pac_man.activities;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ch.ethz.inf.vs.a4.fmorath.pac_man.Game;
+
+public class GameActivity extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}

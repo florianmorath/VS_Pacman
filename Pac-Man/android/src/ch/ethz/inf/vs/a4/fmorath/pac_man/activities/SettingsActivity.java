e9fdaa3978ecb4df6842fb82b285bdafcb0ecf10
Linus <linusfessler@gmail.com>
2016-11-23 13:18:54 +0100

    SettingsActivity

--- Pac-Man/android/src/ch/ethz/inf/vs/a4/fmorath/pac_man/activities/SettingsActivity.java
+++ Pac-Man/android/src/ch/ethz/inf/vs/a4/fmorath/pac_man/activities/SettingsActivity.java
@@ -1,17 +1,57 @@
 package ch.ethz.inf.vs.a4.fmorath.pac_man.activities;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import ch.ethz.inf.vs.a4.fmorath.pac_man.R;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            //PreferenceManager.setDefaultValues(getActivity(), R.xml.preferences, false);
+            addPreferencesFromResource(R.xml.preferences);
+
+            String ipAddressText = String.format(getString(R.string.summary_host_ip_address), getIpAddress());
+            findPreference("host_ip_address").setSummary(ipAddressText);
+        }
+    }
+
+    public static String getIpAddress() {
+        try {
+            Enumeration networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = (NetworkInterface) networkInterfaces.nextElement();
+                Enumeration inetAddresses = networkInterface.getInetAddresses();
+                while (inetAddresses.hasMoreElements()) {
+                    InetAddress inetAddress = (InetAddress) inetAddresses.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address)
+                        return inetAddress.getHostAddress();
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
